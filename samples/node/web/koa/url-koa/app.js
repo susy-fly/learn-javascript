9f470a8fe90a1a0086bdab24c58dc5547f3852c6
shefy1 <1912510604@qq.com>
2021-11-28 21:56:20 +0800

    koa-bodyparser解析request koa-router处理url映射

--- samples/node/web/koa/url-koa/app.js
+++ samples/node/web/koa/url-koa/app.js
@@ -1,7 +1,8 @@
 const Koa = require('koa');
 
+// 解析原始request请求
 const bodyParser = require('koa-bodyparser');
-
+// require（'koa-router'）返回的是函数
 const router = require('koa-router')();
 
 const app = new Koa();
@@ -12,7 +13,10 @@ app.use(async (ctx, next) => {
     await next();
 });
 
+// post请求通常会发送一个表单 或 JSON 作为request的body发送 无论Node.js的原始request对象 还是koa提供的request对象 都不提供解析reuqest的body功能
+// 引入中间件（koa-bodyparser）来解析原始request请求 然后把解析的参数 绑定到ctx.request.body中
 // parse request body:
+// 注意位置 需要放在router之前被注册到app对象上
 app.use(bodyParser());
 
 // add url-route:
